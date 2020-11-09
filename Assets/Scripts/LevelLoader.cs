df9e23f5c914be5ceaabe824dbdabe4bb0806da4
DHotdog85 <dhayes506@hotmail.com>
2020-11-08 20:45:47 -0500

    Project started; Splash screen created; Start screen started;

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private int currentSceneIndex;
+    [SerializeField] private int loadDelayInSeconds = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(SceneLoadAfterDelay());
+        }
+    }
+
+    IEnumerator SceneLoadAfterDelay()
+    {
+        yield return new WaitForSeconds(loadDelayInSeconds);
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
