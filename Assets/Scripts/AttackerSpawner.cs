5a1c881996f1373a9385482a27a056784f13294c
DHotdog85 <dhayes506@hotmail.com>
2020-12-17 22:08:31 -0500

    Cleaned up Lizard animation and created Attacker Spawner.

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    private bool spawn = true;
+    [SerializeField] private float spawnDelayMin = 1f;
+    [SerializeField] private float spawnDelayMax = 5f;
+    [SerializeField] private Attacker attackerPrefab;
+    
+    
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(spawnDelayMin, spawnDelayMax));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(
+            attackerPrefab,
+            transform.position,
+            quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+}
