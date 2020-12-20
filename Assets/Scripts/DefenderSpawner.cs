03bf587c7f19ebdfd47967be4c5cd14ac757906f
DHotdog85 <dhayes506@hotmail.com>
2020-12-20 17:26:21 -0500

    Spawn defenders as a grid placement; rebuilt animations as parent-child objects;

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 
 public class DefenderSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject defender;
-    
+    private Defender defender;
+
     private void OnMouseDown()
     {
         SpawnDefender(GetSquareClicked());
+    }
+
+    public void SetSelectedDefender(Defender defenderToSelect)
+    {
+        defender = defenderToSelect;
     }
 
     private Vector2 GetSquareClicked()
     {
         Vector2 clickPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPosition = Camera.main.ScreenToWorldPoint(clickPosition);
-        return worldPosition;
+        Vector2 gridPosition = SnapToGrid(worldPosition);
+        return gridPosition;
     }
 
-    private void SpawnDefender(Vector2 worldPosition)
+    private Vector2 SnapToGrid(Vector2 rawWorldPosition)
     {
-        GameObject newDefender = Instantiate(defender, worldPosition, Quaternion.identity) as GameObject;
+        float newX = Mathf.RoundToInt(rawWorldPosition.x);
+        float newY = Mathf.RoundToInt(rawWorldPosition.y);
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 roundedPosition)
+    {
+        Defender newDefender = Instantiate(defender, roundedPosition, Quaternion.identity) as Defender;
     }
 }
